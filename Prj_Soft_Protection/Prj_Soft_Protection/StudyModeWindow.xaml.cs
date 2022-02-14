2de5cbca2f6074b0f966a988f22fdc1afca398dd
Nikita Horobets <cloudedlegends1@gmail.com>
2022-02-14 10:23:24 +0200

    For PC usage

--- Prj_Soft_Protection/Prj_Soft_Protection/StudyModeWindow.xaml.cs
+++ Prj_Soft_Protection/Prj_Soft_Protection/StudyModeWindow.xaml.cs
@@ -21,72 +21,120 @@ namespace Prj_Soft_Protection
     /// </summary>
     public partial class StudyModeWindow : Window
     {
-        static int TriesNum = 0, KeysCount = 0;
-        static string ControlText = "";
+        static int TriesNum = 1, KeysCount = 0;
+
         static Stopwatch stopWatch = new Stopwatch();
         static List<long> Intervals = new List<long>();
+
         public StudyModeWindow()
         {
             InitializeComponent();
-            ControlText = VerifField.Text;
+
         }
-        private void TextBox_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            if (CountProtection.SelectedItem != null)
-                MessageBox.Show("Please choose tries count.", "ComboxError", MessageBoxButton.OK);
-            else
-            {
-                KeysCount++;
-                
-                if (KeysCount == 1)
-                    stopWatch.Start();
-                else
-                {
-                    long bufftime = stopWatch.ElapsedMilliseconds;
-                    Intervals.Add(bufftime);
-                    if ((InputField.Text.ToString())[KeysCount-1] != (ControlText.ToString())[KeysCount-1])
-                    {
-                        Intervals.Clear();
-                        KeysCount = 0;
-                        InputField.Text = "";
-                        MessageBox.Show("Input error, try isn't counted", "Input error", MessageBoxButton.OK);
-                        stopWatch.Stop();
-                    }
-                }
-            }
-        }
+
+
+
 
         private void MainMethod(int Tries)
         {
-            Intervals.Clear(); KeysCount = 0; TriesNum = 0; InputField.Text = "";
-            stopWatch.Stop();
-            while (TriesNum < Tries)
+            InputField.Text = ""; ;
+            if (TriesNum != Tries)
             {
-                if (KeysCount == (ControlText.ToString()).Length)
+                stopWatch.Stop();
+                //Мат. часть будет тут...
+                double alpha = 0.05;
+                int check = 1;
+                for (int i = 0; i < Intervals.Count; i++)
                 {
-                    stopWatch.Stop();
-                    //Мат. часть будет тут...
-                    MessageBox.Show("Successfull try, gratz", "Success", MessageBoxButton.OK);
+                    double y_i = Intervals[i];
+                    double M_y = 0, M_y2 = 0, M2_y = 0, D = 0, Tp;
+                    for (int j = 0; j < Intervals.Count; j++)
+                    {
+                        if (j == i)
+                            continue;
+                        else
+                        {
+                            M_y += Intervals[j];
+                            M2_y += Intervals[j];
+                            M_y2 += Math.Pow(Intervals[j], 2);
+                        }
+                        D = M_y2 - Math.Pow(M2_y, 2);
+                        Tp = (y_i - M_y) / D;
+
+                        if (Tp > 1.860)
+                            check *= 0;
+                    }
+                }
+                if (check == 0)
+                {
+                    MessageBox.Show("Not a successfull try due to user being dumb" + "\nTries: " + TriesNum, "Success", MessageBoxButton.OK);
+                    Intervals.Clear();
+                    KeysCount = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Successfull try, gratz" + "\nKeys: " + KeysCount + "\nTries: " + TriesNum, "Success", MessageBoxButton.OK);
                     Intervals.Clear();
                     KeysCount = 0;
                     TriesNum++;
                 }
+                //А тут кончается
             }
-            //Вторая мат. часть тут...
-            //И запись в файл
-            MessageBox.Show("Tries кончились, иди спать");
+            else
+            {
+                //Вторая мат. часть тут...
+                //И запись в файл
+                MessageBox.Show("Tries кончились, иди спать");
+            }
         }
 
 
 
-        private void CountProtection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ErrorMsg()
         {
-            if (CountProtection.Text != "")
-              MainMethod(int.Parse(CountProtection.Text));
+            Intervals.Clear();
+            KeysCount = 0;
+            InputField.Text = "";
+            MessageBox.Show("Something went terribly wrong i have no clue what it is", "Error", MessageBoxButton.OK);
+            stopWatch.Stop();
+        }
+
+        private void InputField_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (CountProtection.SelectedItem == null)
+            {
+                InputField.Text = "";
+                MessageBox.Show("Please choose tries count.\n" + CountProtection.Text, "ComboxError", MessageBoxButton.OK);
+            }
+            else
+            {
+                KeysCount++;
+                MessageBox.Show(InputField.Text);
+                SymbolCount.Content = KeysCount.ToString();
+                if (KeysCount == 1)
+                {
+                    stopWatch.Start();
+                    if (InputField.Text[KeysCount-1] != VerifField.Text[KeysCount-1] )
+                        ErrorMsg();
+                }
+                else if (KeysCount == VerifField.Text.Length)
+                {
+                    MainMethod(int.Parse(CountProtection.Text));
+                }
+                else
+                {
+                    long bufftime = stopWatch.ElapsedMilliseconds;
+                    Intervals.Add(bufftime);
+                    if (InputField.Text[KeysCount] != VerifField.Text[KeysCount] )
+                        ErrorMsg();
+                }
+            }
         }
 
         private void CloseStudyMode_Click(object sender, RoutedEventArgs e)
         {
+            Intervals.Clear(); KeysCount = 0; TriesNum = 0; InputField.Text = "";
+            stopWatch.Stop();
             MainWindow nwc = new MainWindow();
             Hide();
             nwc.Show();
