86da614d0e012eafd4845bac864503292eee8cfe
Nikita Horobets <cloudedlegends1@gmail.com>
2022-02-14 20:32:47 +0200

    Done with training window

--- Prj_Soft_Protection/Prj_Soft_Protection/StudyModeWindow.xaml.cs
+++ Prj_Soft_Protection/Prj_Soft_Protection/StudyModeWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.IO;
 
 
 
@@ -21,10 +22,10 @@ namespace Prj_Soft_Protection
     /// </summary>
     public partial class StudyModeWindow : Window
     {
-        static int TriesNum = 1, KeysCount = 0;
-
+        static int TriesNum = 1, KeysCount = 0, TriesTotal = 0;
+        static string CodeText = "";
         static Stopwatch stopWatch = new Stopwatch();
-        static List<long> Intervals = new List<long>();
+        static double[,] Intervals;
 
         public StudyModeWindow()
         {
@@ -32,112 +33,138 @@ namespace Prj_Soft_Protection
 
         }
 
-
-
-
-        private void MainMethod(int Tries)
-        {
-            InputField.Text = ""; ;
-            if (TriesNum != Tries)
-            {
-                stopWatch.Stop();
-                //Мат. часть будет тут...
-                double alpha = 0.05;
-                int check = 1;
-                for (int i = 0; i < Intervals.Count; i++)
-                {
-                    double y_i = Intervals[i];
-                    double M_y = 0, M_y2 = 0, M2_y = 0, D = 0, Tp;
-                    for (int j = 0; j < Intervals.Count; j++)
-                    {
-                        if (j == i)
-                            continue;
-                        else
-                        {
-                            M_y += Intervals[j];
-                            M2_y += Intervals[j];
-                            M_y2 += Math.Pow(Intervals[j], 2);
-                        }
-                        D = M_y2 - Math.Pow(M2_y, 2);
-                        Tp = (y_i - M_y) / D;
-
-                        if (Tp > 1.860)
-                            check *= 0;
-                    }
-                }
-                if (check == 0)
-                {
-                    MessageBox.Show("Not a successfull try due to user being dumb" + "\nTries: " + TriesNum, "Success", MessageBoxButton.OK);
-                    Intervals.Clear();
-                    KeysCount = 0;
-                }
-                else
-                {
-                    MessageBox.Show("Successfull try, gratz" + "\nKeys: " + KeysCount + "\nTries: " + TriesNum, "Success", MessageBoxButton.OK);
-                    Intervals.Clear();
-                    KeysCount = 0;
-                    TriesNum++;
-                }
-                //А тут кончается
-            }
-            else
-            {
-                //Вторая мат. часть тут...
-                //И запись в файл
-                MessageBox.Show("Tries кончились, иди спать");
-            }
-        }
-
-
-
         private void ErrorMsg()
         {
-            Intervals.Clear();
             KeysCount = 0;
             InputField.Text = "";
-            MessageBox.Show("Something went terribly wrong i have no clue what it is", "Error", MessageBoxButton.OK);
+            MessageBox.Show("Something went wrong...", "Error", MessageBoxButton.OK);
             stopWatch.Stop();
         }
 
         private void InputField_KeyDown(object sender, KeyEventArgs e)
         {
-            if (CountProtection.SelectedItem == null)
-            {
-                InputField.Text = "";
-                MessageBox.Show("Please choose tries count.\n" + CountProtection.Text, "ComboxError", MessageBoxButton.OK);
-            }
-            else
+            if (TriesTotal == 0 || CodeText.Length == 0)
+            { ErrorMsg(); e.Handled = true; }
+            ////
+            else if (e.Key.ToString().ToLower() == CodeText[KeysCount].ToString())
             {
                 KeysCount++;
-                MessageBox.Show(InputField.Text);
                 SymbolCount.Content = KeysCount.ToString();
                 if (KeysCount == 1)
                 {
-                    stopWatch.Start();
-                    if (InputField.Text[KeysCount-1] != VerifField.Text[KeysCount-1] )
-                        ErrorMsg();
+                    stopWatch.Restart();
                 }
-                else if (KeysCount == VerifField.Text.Length)
+                else if (KeysCount == CodeText.Length)
                 {
-                    MainMethod(int.Parse(CountProtection.Text));
+                    Intervals[TriesNum - 1, KeysCount - 1] = stopWatch.ElapsedMilliseconds / 1000.0;
+                    stopWatch.Stop();
+                    int check = IntervalCheck(TriesNum - 1);
+                    if (check == 1)
+                    {
+                        MessageBox.Show("Successfull try" + "\nKeys: " + KeysCount + "\nTries: " + TriesNum, "Success", MessageBoxButton.OK);
+                        InputField.Text = "";
+                        e.Handled = true;
+                        KeysCount = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not a successfull try, Tp > Tp from the table (alpha = 0.05)" + "\nKeys: " + KeysCount + "\nTries: " + TriesNum, "Success", MessageBoxButton.OK);
+                        InputField.Text = "";
+                        e.Handled = true;
+                        KeysCount = 0;
+                    }
+                    if (TriesNum == TriesTotal)
+                    {
+                        stopWatch.Stop();
+                        MessageBox.Show("Tries кончились");
+                        e.Handled = true;
+                        InputField.Text = "";
+                        InputField.IsEnabled = false;
+                    }
+                    TriesNum++;
                 }
                 else
                 {
-                    long bufftime = stopWatch.ElapsedMilliseconds;
-                    Intervals.Add(bufftime);
-                    if (InputField.Text[KeysCount] != VerifField.Text[KeysCount] )
-                        ErrorMsg();
+                    Intervals[TriesNum - 1, KeysCount - 1] = stopWatch.ElapsedMilliseconds / 1000.0;
+                    stopWatch.Restart();
                 }
             }
+            ////
+            else
+            { ErrorMsg(); e.Handled = true; }
+        }
+
+        private void CountProtection_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9)
+            {
+                TriesTotal = int.Parse(e.Key.ToString().Substring(1, 1));
+                Intervals = new double[TriesTotal, CodeText.Length];
+                MessageBox.Show(TriesTotal + ":" + CodeText);
+            }
+            else
+                ErrorMsg();
+        }
+
+        private void Verified_KeyDown(object sender, KeyEventArgs e)
+        {
+            CodeText += e.Key.ToString().ToLower();
         }
 
         private void CloseStudyMode_Click(object sender, RoutedEventArgs e)
         {
-            Intervals.Clear(); KeysCount = 0; TriesNum = 0; InputField.Text = "";
-            stopWatch.Stop();
+
+            using (StreamWriter stream = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/IntervalsTime.txt"))
+            {
+               
+                for (int i = 0; i < Intervals.GetLength(0); i++)
+                {
+                    for (int j = 1; j < Intervals.GetLength(1); j++)
+                        stream.Write(Intervals[i, j] + " ");
+                    stream.WriteLine();
+                }
+            }
+            KeysCount = 0; TriesNum = 0; InputField.Text = "";
             MainWindow nwc = new MainWindow();
             Hide();
             nwc.Show();
+        }
+
+        private int IntervalCheck(int tries)
+        {
+            int check = 1; double M = 0, D = 0, Dsqrt = 0, Tp;
+            for (int j = 1; j < Intervals.GetLength(1); j++)
+            {
+                double y = Intervals[tries, j]; 
+                for (int k = 1; k < Intervals.GetLength(1); k++) 
+                {
+                    if (k == j)
+                        continue;
+                    else
+                        M += Intervals[tries, k]; 
+                }
+                M /= Intervals.GetLength(1) - 1;
+                D = getD(y, tries, M); 
+                Dsqrt = Math.Sqrt(D); 
+                Tp = Math.Abs((y - M) / Dsqrt);
+                if (Tp > 2.306) //df = 8, alpha = 0.05
+                { check *= 0; }
+                MessageBox.Show(Tp.ToString());
+            } 
+            return check;
+        }
+        private double getD(double y, int j, double M)
+        {
+            double D = 0;
+            for (int i = 0; i < Intervals.GetLength(1); i++)
+            {
+                if (Intervals[j, i] == y)
+                    continue; 
+                else
+                    D += (Intervals[j, i] - M) * (Intervals[j, i] - M);
+            }
+            D /= Intervals.GetLength(1) - 2;
+            return D;
         }
     }
 }
