fb8433917e0facd7e7923daf14e6a12981e2a4ea
Nikita Horobets <cloudedlegends1@gmail.com>
2022-04-20 18:08:15 +0300

    Added lab4

---
+++ LabFour/ConnectToSQLServer/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System.Windows;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Windows.Controls;
+
+namespace ConnectToSQLServer
+{
+    public partial class MainWindow : Window
+    {
+        string connectionString = null;
+        SqlConnection connection = null;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //MessageBox.Show(connectionString);
+            GetFirstQ();
+            GetSecondQ();
+            GetThirdQ();
+            GetSeventhQ();
+        }
+
+        private void GetAndDhowData(string SQLQuery, DataGrid dataGrid)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(SQLQuery, connection);
+            adapter = new SqlDataAdapter(command);
+            DataTable Table = new DataTable();
+            adapter.Fill(Table);
+            dataGrid.ItemsSource = Table.DefaultView;
+            connection.Close();
+        }
+
+        private void GetFirstQ()
+        {
+            string sqlQ = "SELECT Alias, Clients.Adress, Clients.PhoneNum, Crimetype " +
+                          "FROM ClienTs " +
+                          "INNER JOIN Journal ON Journal.ClD = Clients.CID " +
+                          " INNER JOIN Cases ON Journal.CaseID = Cases.CaseID " +
+                          "ORDER BY CrimeType; ";
+            try
+            {
+                GetAndDhowData(sqlQ, FirstQ);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+
+        private void GetSecondQ()
+        {
+            string sqlQ = "SELECT Alias AS [Name, Surname], Clients.BDate AS [Date of Birth], Clients.Adress, Clients.PhoneNum, Crimetype AS Crime " +
+                          "FROM ClienTs " +
+                          "INNER JOIN Journal ON Journal.ClD = Clients.CID " +
+                          "INNER JOIN Cases ON Journal.CaseID = Cases.CaseID " +
+                          "WHERE ClienTs.BDate >= '2004-01-01' " +
+                          "ORDER BY Alias; ";
+            try
+            {
+                GetAndDhowData(sqlQ, SecondQ);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void GetThirdQ()
+        {
+            string sqlQ = "SELECT Alias, Clients.Adress, Clients.PhoneNum, Crimetype AS Crime, SentenceW  + ' years' AS [Worst Possible Scenario] " +
+                            "FROM ClienTs " +
+                          "INNER JOIN Journal ON Journal.ClD = Clients.CID " +
+                           "INNER JOIN Cases ON Journal.CaseID = Cases.CaseID " +
+                          "WHERE IsYr = 1 AND SentenceW >= 1 " +
+                          "ORDER BY Alias;";
+            try
+            {
+                GetAndDhowData(sqlQ, ThirdQ);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+       
+        
+        private void GetSeventhQ()
+        {
+            string sqlQ = "SELECT SUM(Fee) AS [Total Fee Number] FROM Cases;";
+            try
+            {
+                GetAndDhowData(sqlQ, SeventhQ);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win = new Window1();
+            Close();
+            win.Show();
+        }
+    }
+}
+
---
+++ LabFour/ConnectToSQLServer/Window1.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectToSQLServer
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        string connectionString = null;
+        SqlConnection connection = null;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        public Window1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            GetFifthQ();
+            GetFourthQ();
+            GetSixthQ();
+        }
+        private void GetAndDhowData(string SQLQuery, DataGrid dataGrid)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(SQLQuery, connection);
+            adapter = new SqlDataAdapter(command);
+            DataTable Table = new DataTable();
+            adapter.Fill(Table);
+            dataGrid.ItemsSource = Table.DefaultView;
+            connection.Close();
+        }
+        private void GetFifthQ()
+        {
+            string sqlQ = "SELECT Journal.CaseID, Attorneys.NameSurname AS Attorney, Attorneys.PhoneNum AS [Attorney's Phone Number], " +
+      " Alias AS Client, Clients.PhoneNum AS[Clien's Phone Number],   CASE IsYr  WHEN 1 THEN SentenceW + ' years' WHEN 0 THEN '$' + SentenceW END " +
+ " AS[Worst Scenario],  CASE IsYr  WHEN 1 THEN SentenceGot + ' years' WHEN 0 THEN '$' + SentenceGot END  AS Result, Fee " +
+"FROM Journal INNER JOIN Clients ON Journal.ClD = Clients.CID INNER JOIN Cases ON Journal.CaseID = Cases.CaseID " +
+ "INNER JOIN Attorneys ON Attorneys.AttID = Journal.AttID WHERE SentenceGot < SentenceW ORDER BY Journal.CaseID; ";
+            try
+            {
+                GetAndDhowData(sqlQ, FifthQ);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void GetSixthQ()
+        {
+            string sqlQ = "SELECT Attorneys.NameSurname AS [Name, Surname], Attorneys.Adress, Attorneys.PhoneNum AS [Phone Number], Education, " +
+ "Clients.Alias[Client], Journal.CaseClosed AS[Closing Date], Fee AS[Earned Fee] FROM Attorneys  INNER JOIN Journal ON Journal.AttID = Attorneys.AttID " +
+"INNER JOIN Clients ON Journal.ClD = Clients.CID INNER JOIN Cases ON Journal.CaseID = Cases.CaseID WHERE IsOver = 1 ORDER BY Attorneys.NameSurname;  ";
+            try
+            {
+                GetAndDhowData(sqlQ, SixthQ);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void GetFourthQ()
+        {
+            string sqlQ = "SELECT Journal.CaseID, Attorneys.NameSurname AS Attorney, Attorneys.PhoneNum AS[Attorney's Phone Number], " +
+                       " Alias AS Client, Clients.PhoneNum AS[Clien's Phone Number], CrimeType AS Crime, " +
+                      " CaseClosed AS[Case closing Date],   CASE IsYr    WHEN 1 THEN SentenceGot + ' years'  WHEN 0 THEN '$' + SentenceGot END " +
+                       "  AS Result, Fee FROM Journal INNER JOIN Clients ON Journal.ClD = Clients.CID  INNER JOIN Cases ON Journal.CaseID = Cases.CaseID " +
+                          "  INNER JOIN Attorneys ON Attorneys.AttID = Journal.AttID WHERE IsOver = 1 ORDER BY Journal.CaseID; ";
+            try
+            {
+                GetAndDhowData(sqlQ, FourthQ);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
